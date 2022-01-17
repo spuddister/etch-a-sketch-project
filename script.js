92ad9f7a118bb742fd3dbd9cee274bd68ce9367b
Sam Puddister <sampuddister@gmail.com>
2022-01-17 14:50:24 -0500

    Built colourChange function which changes the grid square background colour on mouseover. Added clear button to revert changes.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 
 const gridContainer = document.getElementById('grid-container');
-let gridSize = 12;
-
+let gridSize = 70;
 for (i = 0; i < gridSize*gridSize; i++) {
     addSquare();
 }
-
+let gridElements = Array.from(document.getElementsByClassName('grid-square'));
 
 function addSquare() {
     let gridSquare = document.createElement('div');
@@ -14,8 +13,18 @@ function addSquare() {
     gridContainer.appendChild(gridSquare);
 }
 
-// let squares = document.getElementsByClassName('grid-square');
-// squares.forEach(square => {
-//     console.log(square);
-// });
-// console.log(squares[0]);
+//Adding event listeners to all grid squares for changing background color
+gridElements.forEach(element => {
+    element.addEventListener("mouseover", colourChange);
+});
+
+function colourChange(e) {
+    e.target.style.backgroundColor = "black";
+}
+
+//Clear all grid squares via clear button functionality
+const clearBtn = document.getElementById('clear').addEventListener('click', function(){
+    gridElements.forEach(element => {
+        element.style.backgroundColor = "white";
+    });
+});
