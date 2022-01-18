14261d7b52b017f14cc3106ef1c8fe81598f368c
Sam Puddister <sampuddister@gmail.com>
2022-01-17 23:12:22 -0500

    Added rainbow mode to paint tiles a random RGB value on mouseover. Added a slider bar to adjust the grid size and a div to display value, have not connected output to grid yet.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
 const gridContainer = document.getElementById('grid-container');
-let gridSize = 70;
-for (i = 0; i < gridSize*gridSize; i++) {
-    addSquare();
-}
+let rainbow = false;
+let eraser = false;
+let gridSize = 8;
+for (i = 0; i < gridSize*gridSize; i++) addSquare();
 let gridElements = Array.from(document.getElementsByClassName('grid-square'));
 
 function addSquare() {
@@ -15,16 +15,46 @@ function addSquare() {
 
 //Adding event listeners to all grid squares for changing background color
 gridElements.forEach(element => {
-    element.addEventListener("mouseover", colourChange);
+    element.addEventListener('mouseover', painter);
 });
 
-function colourChange(e) {
-    e.target.style.backgroundColor = "black";
+//Painter function alters the background colour of the grid squares depending on which modes are selected
+function painter(e) {
+    if (eraser) {
+        e.target.style.backgroundColor = 'white';
+    } else if (rainbow) {
+        e.target.style.backgroundColor = 'rgb(' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ',' + Math.floor(Math.random()*255) + ')';
+    } else {
+        e.target.style.backgroundColor = 'black';
+    }
 }
 
+
+//BUTTONS AND SLIDERS
 //Clear all grid squares via clear button functionality
 const clearBtn = document.getElementById('clear').addEventListener('click', function(){
     gridElements.forEach(element => {
-        element.style.backgroundColor = "white";
+        element.style.backgroundColor = 'white';
     });
 });
+
+const rainbowBtn = document.getElementById('rainbow-mode').addEventListener('click', function(){
+    if (rainbow) {
+        rainbow = false;
+    } else {
+        rainbow = true;
+    }
+});
+
+const eraserBtn = document.getElementById('eraser').addEventListener('click', function(){
+    if (eraser) {
+        eraser = false;
+    } else {
+        eraser = true;
+    }
+});
+
+const gridSizeSlider = document.getElementById('grid-size-slider').addEventListener('input', function(){
+    gridSize = this.value;
+    document.getElementById('grid-size-value').textContent = gridSize;
+})
